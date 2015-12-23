8e711b3460f796e3b85ebf1e0aaa15ed3b04b0af
Yin Lingjun <yin_lingjun@hithinksoft.com>
2015-12-23 09:16:36 +0800

    redis

---
+++ src/test/java/TestRedis.java
@@ -0,0 +1,35 @@
+import org.junit.Before;
+import org.junit.Test;
+import redis.clients.jedis.Jedis;
+
+/**
+ * Created by ksdhc on 2015/12/22.
+ */
+public class TestRedis {
+    private Jedis jedis;
+
+    @Before
+    public void setup() {
+        //连接redis服务器，192.168.0.100:6379
+        jedis = new Jedis("127.0.0.1", 6379);
+        //权限认证
+    }
+
+    @Test
+    public void testString() {
+        //-----添加数据----------
+        jedis.set("name", "xinxin");//向key-->name中放入了value-->xinxin
+        System.out.println(jedis.get("name"));//执行结果：xinxin
+
+        jedis.append("name", " is my lover"); //拼接
+        System.out.println(jedis.get("name"));
+
+        jedis.del("name");  //删除某个键
+        System.out.println(jedis.get("name"));
+        //设置多个键值对
+        jedis.mset("name", "liuling", "age", "23", "qq", "476777XXX");
+        jedis.incr("age"); //进行加1操作
+        System.out.println(jedis.get("name") + "-" + jedis.get("age") + "-" + jedis.get("qq"));
+
+    }
+}
